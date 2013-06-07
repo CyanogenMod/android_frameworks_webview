1d6f99d5c9c776f5cfc638b0e020fb08dd536dca
Torne (Richard Coles) <torne@google.com>
2013-06-07 12:08:27 +0100

    Add tool to check that we jarjar'ed everything.
    Add a tool to verify that after jarjar has run, all the classes
    remaining are in whitelisted packages. This will ensure that we don't
    accidentally introduce a conflicting class definition into the system
    classpath by failing to rename a new dependency. Make the built module
    depend on this check step so that the build will fail if the check
    doesn't pass.
    
    Bug: 8030113
    Change-Id: I92fc75eeee644a3eb05a13799ae398be20c286b3

--- chromium/Android.mk
+++ chromium/Android.mk
@@ -98,6 +98,24 @@ LOCAL_JAVACFLAGS := -Xlint:unchecked -Werror
 
 include $(BUILD_JAVA_LIBRARY)
 
+ifneq ($(strip $(LOCAL_JARJAR_RULES)),)
+# Add build rules to check that the jarjar'ed jar only contains whitelisted
+# packages. Only enable this when we are running jarjar.
+LOCAL_JAR_CHECK_WHITELIST := $(LOCAL_PATH)/package_whitelist.txt
+
+jar_check_ok := $(intermediates.COMMON)/jar_check_ok
+$(jar_check_ok): PRIVATE_JAR_CHECK := $(LOCAL_PATH)/tools/jar_check.py
+$(jar_check_ok): PRIVATE_JAR_CHECK_WHITELIST := $(LOCAL_JAR_CHECK_WHITELIST)
+$(jar_check_ok): $(full_classes_jarjar_jar) $(LOCAL_PATH)/tools/jar_check.py $(LOCAL_JAR_CHECK_WHITELIST)
+	@echo Jar check: $@
+	$(hide) $(PRIVATE_JAR_CHECK) $< $(PRIVATE_JAR_CHECK_WHITELIST)
+	$(hide) touch $@
+
+$(LOCAL_BUILT_MODULE): $(jar_check_ok)
+endif
+
+
+
 # Native support library (libwebviewchromium_plat_support.so) - does NOT link
 # any native chromium code.
 include $(CLEAR_VARS)
