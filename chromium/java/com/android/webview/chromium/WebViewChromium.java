058714d7ae16bbfc096d9b4e0423efeb897bae68
Jonathan Dixon <joth@google.com>
2013-07-19 20:12:41 -0700

    Hookup WebViewChromium.setBackgroundColor
    Change-Id: Ie22b18beef01a4a6d539adf3579d56615de7a820

--- chromium/java/com/android/webview/chromium/WebViewChromium.java
+++ chromium/java/com/android/webview/chromium/WebViewChromium.java
@@ -865,7 +865,7 @@ class WebViewChromium implements WebViewProvider,
 
     @Override
     public void setBackgroundColor(int color) {
-        UnimplementedWebViewApi.invoke();
+        mAwContents.setBackgroundColor(color);
     }
 
     @Override
