55aff944126d8f2dc4020ec88405a93919579db2
Bo Liu <boliu@google.com>
2014-02-28 07:14:59 -0800

    Make executeHardwareAction avaiable to AwContents
    Removed wrapping all of onDetachedFromWindow with executeHardwareAction,
    since upstream code will have finer control on what code to wrap
    executeHardwareAction in.
    
    Also take this opportunity to clean up native public interface.
    
    This is the first of a 3-sided patch. The upstream change is at
    https://codereview.chromium.org/185133003/
    
    Change-Id: Ie791842a0becd8189dd8d52a027b75b8ee34211a

--- chromium/plat_support/graphics_utils.cpp
+++ chromium/plat_support/graphics_utils.cpp
@@ -82,36 +82,10 @@ void ReleasePixels(AwPixelInfo* pixels) {
   delete static_cast<PixelInfo*>(pixels);
 }
 
-jobject CreatePicture(JNIEnv* env, SkPicture* picture) {
-  jclass clazz = env->FindClass("android/graphics/Picture");
-  jmethodID constructor = env->GetMethodID(clazz, "<init>", "(IZ)V");
-  ALOG_ASSERT(clazz);
-  ALOG_ASSERT(constructor);
-  return env->NewObject(clazz, constructor, picture, false);
-}
-
-bool IsSkiaVersionCompatible(SkiaVersionFunction function) {
-  bool compatible = false;
-  if (function && function == &SkGraphics::GetVersion) {
-    int android_major, android_minor, android_patch;
-    SkGraphics::GetVersion(&android_major, &android_minor, &android_patch);
-
-    int chromium_major, chromium_minor, chromium_patch;
-    (*function)(&chromium_major, &chromium_minor, &chromium_patch);
-
-    compatible = android_major == chromium_major &&
-                 android_minor == chromium_minor &&
-                 android_patch == chromium_patch;
-  }
-  return compatible;
-}
-
 jint GetDrawSWFunctionTable(JNIEnv* env, jclass) {
   static const AwDrawSWFunctionTable function_table = {
       &GetPixels,
       &ReleasePixels,
-      &CreatePicture,
-      &IsSkiaVersionCompatible,
   };
   return reinterpret_cast<jint>(&function_table);
 }
