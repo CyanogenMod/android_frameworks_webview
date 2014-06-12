2176bfd50835911c98e1581eb40f089aa921b125
Richard Coles <torne@google.com>
2014-06-12 17:27:15 +0000

    Revert "Add libaddressinput to the merged project list."
    This reverts commit 13e2ded0ee997ac247ce18be587125c149f9c4c9.
    
    Change-Id: If3f87f147a26a6d552483f34e25a039a7f830598

--- chromium/tools/merge_common.py
+++ chromium/tools/merge_common.py
@@ -42,7 +42,6 @@ THIRD_PARTY_PROJECTS_WITH_FULL_HISTORY = [
     'third_party/freetype',
     'third_party/icu',
     'third_party/leveldatabase/src',
-    'third_party/libaddressinput/src/cpp',
     'third_party/libjingle/source/talk',
     'third_party/libphonenumber/src/phonenumbers',
     'third_party/libphonenumber/src/resources',
