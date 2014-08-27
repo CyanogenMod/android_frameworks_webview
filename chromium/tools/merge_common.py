5d6864f2d617bb5a0e143f0c8d7f7caa58bfe5cd
Torne (Richard Coles) <torne@google.com>
2014-08-27 11:13:28 +0100

    Remove openssl from merged projects list.
    OpenSSL has been replaced by boringssl upstream and no longer exists in
    DEPS.
    
    Change-Id: Idb3ad563ead2a48b98196359a65bde9fa52da90e
    (cherry picked from commit 835aeb90a376699f177d2da20ab83387b578ebc6)

--- chromium/tools/merge_common.py
+++ chromium/tools/merge_common.py
@@ -52,7 +52,6 @@ THIRD_PARTY_PROJECTS_WITH_FULL_HISTORY = [
     'third_party/libyuv',
     'third_party/mesa/src',
     'third_party/openmax_dl',
-    'third_party/openssl',
     'third_party/opus/src',
     'third_party/ots',
     'third_party/sfntly/cpp/src',
