bc32c8b48888837f0482645b3a93aaa7fa813f8e
Tao Bai <michaelbai@google.com>
2014-09-02 14:10:38 -0700

    Implement FileChooserParams.createIntent and parseResult
    BUG:17253647, 16624450
    
    Change-Id: Ief3861fafabf2799f1d3575b30e2ceddbbb0f0d9

--- chromium/java/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ chromium/java/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -22,7 +22,9 @@ import android.app.ActivityManager;
 import android.app.ActivityThread;
 import android.content.ComponentCallbacks2;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.os.FileUtils;
 import android.os.Looper;
@@ -348,6 +350,11 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
                     // TODO: Add @Override.
                     public void enableSlowWholeDocumentDraw() {
                         WebViewChromium.enableSlowWholeDocumentDraw();
+                    }
+
+                    @Override
+                    public Uri[] parseFileChooserResult(int resultCode, Intent intent) {
+                        return FileChooserParamsAdapter.parseFileChooserResult(resultCode, intent);
                     }
                 };
             }
